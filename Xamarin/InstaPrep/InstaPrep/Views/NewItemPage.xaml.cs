cce3e69bad5b502e4fc196a4659aa2c4a1e61b63
Adam Lepley <alepley@live.com>
2022-06-19 19:12:26 -0500

    Initial Xamarin Forms tabbed template generated files

---
+++ Xamarin/InstaPrep/InstaPrep/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using InstaPrep.Models;
+using InstaPrep.ViewModels;
+
+namespace InstaPrep.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
