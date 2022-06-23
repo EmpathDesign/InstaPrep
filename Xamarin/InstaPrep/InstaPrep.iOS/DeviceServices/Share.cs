c97bc17ec50016590596e7a86f4a215aea5957f5
Adam Lepley <adam@empathdesignllc.com>
2022-06-23 12:22:37 -0500

    Implemented Share Dependency Service

---
+++ Xamarin/InstaPrep/InstaPrep.iOS/DeviceServices/Share.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using Foundation;
+using InstaPrep.DeviceServices;
+using InstaPrep.iOS.DeviceServices;
+using UIKit;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(Share))]
+namespace InstaPrep.iOS.DeviceServices
+{
+    public class Share : IShare
+    {
+        public async Task Show(string title, string message, string filePath)
+        {
+            var viewController = GetVisibleViewController();
+            var items = new NSObject[] { NSObject.FromObject(title), NSUrl.FromFilename(filePath), NSString.FromObject(message) };
+
+            var activityController = new UIActivityViewController(items, null);
+
+            if (activityController.PopoverPresentationController != null)
+                activityController.PopoverPresentationController.SourceView = viewController.View;
+
+            await viewController.PresentViewControllerAsync(activityController, true);
+        }
+
+        private UIViewController GetVisibleViewController()
+        {
+            var rootViewController = UIApplication.SharedApplication.KeyWindow.RootViewController;
+
+            if (rootViewController.PresentedViewController == null)
+                return rootViewController;
+
+            if (rootViewController.PresentedViewController is UINavigationController)
+                return ((UINavigationController)rootViewController.PresentedViewController).TopViewController;
+
+            if (rootViewController.PresentedViewController is UITabBarController)
+                return ((UITabBarController)rootViewController.PresentedViewController).SelectedViewController;
+
+            return rootViewController.PresentedViewController;
+        }
+    }
+}
+
--- Xamarin/InstaPrep/InstaPrep/ViewModels/AboutViewModel.cs
+++ Xamarin/InstaPrep/InstaPrep/ViewModels/AboutViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows.Input;
+using InstaPrep.DeviceServices;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 
@@ -10,7 +11,14 @@ namespace InstaPrep.ViewModels
         public AboutViewModel()
         {
             Title = "About";
-            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+            OpenWebCommand = new Command(async () =>
+            {
+                IShare shareDependency = DependencyService.Get<IShare>();
+                await Browser.OpenAsync("https://adamlepley.io/assets/pdf/adam_resume.pdf");
+
+
+                //await shareDependency.Show("Adam Lepley Resume.pdf", "adam_resume.pdf");
+            });
         }
 
         public ICommand OpenWebCommand { get; }
